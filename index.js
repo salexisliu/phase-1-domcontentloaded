8e25ef28f586103b314559d20e268bc9884a0b14
Alexis S Liu <syl1350@gmail.com>
2021-07-25 00:04:45 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+  const replaceText = document.querySelector("#text"); //or "p"
+  replaceText.textContent = "This is really cool!";
+});
+console.log(
+  "thigit ss console log fires when index.js loads before dom cntent loaded is trigered"
+);
