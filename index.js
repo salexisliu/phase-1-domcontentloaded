d4523426a30d6f624cc8526c5a688f62c4d8e189
Alexis S Liu <syl1350@gmail.com>
2021-07-25 22:46:09 -0400

    commit tree 7451d168ed1436318d433de9d39208509f1012fd

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Your code goes here
 document.addEventListener("DOMContentLoaded", function () {
+  
   const replaceText = document.querySelector("#text"); //or "p"
   replaceText.textContent = "This is really cool!";
 });
